cb8f835635725e8cc069cb34d556b6769ce0a60d
Andre Roque <mateusandre1@yahoo.com>
2019-10-09 21:24:21 -0400

    apiRoutes.js is complete

--- app/data/friend.js
+++ app/data/friend.js
@@ -12,9 +12,9 @@ var friends = [
       scores: [2, 2, 5, 1, 3, 5, 2, 1, 3, 1]
     },
     {
-      name: "Anthony Joshua",
+      name: "Robert Downey JR",
       photo:
-        "https://m.media-amazon.com/images/M/MV5BYWFlMmVhZjAtOGMzNS00MzEyLTkzMWEtN2M1MDgzODI5Njk0XkEyXkFqcGdeQXVyNTI5NjIyMw@@._V1_.jpg",
+        "https://i.pinimg.com/originals/a8/22/40/a82240a6f7b4235d5e1213f5198e35dd.jpg",
       scores: [5, 2, 5, 5, 4, 1, 3, 2, 5, 5]
     },
     {
--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -4,13 +4,13 @@ var friends = require("../app/data/friend");
 
 // import express app and make the array of objects in json format
 module.exports = function (app) {
-    app.get("/api/friends", function (req, red){
+    app.get("/api/friends", function (req, red) {
         res.json(friends);
     });
 }
 
 // main logic to post and make an algorythm to decide friends best match
-app.post("/api/friends", function (req, res){
+app.post("/api/friends", function (req, res) {
 
     let totalDiff = 0;
 
@@ -19,5 +19,36 @@ app.post("/api/friends", function (req, res){
         photo: "",
         friendDiff: 1000
     };
+
+    let matchData = req.body;
+    let matchName = matchData.name;
+    let matchScores = matchData.scores;
+
+    let b = matchScores.map(function (item) {
+        return parseInt(item, 10);
+    });
+
+    matchData = {
+        name: req.body.name,
+        photo: req.body.photo,
+        scores: b
+    };
+
+    console.log(`name: ${matchName}\nMatch Score: ${matchScores}\n`);
+
+    let total = b.reduce((a, b) => a + b, 0);
+
+    console.log(`Sum of match score: ${total}\nBest Match diff ${bestMatch.friendDiff}\n`);
+
+    for (let i = 0; i < friends.length; i++) {
+        bestMatch.name = friends[i].name;
+        bestMatch.photo = friends[i].photo;
+        bestMatch.friendDiff = totalDiff;
+    }
+
+    console.log(`Total Difference: ${totalDiff}`);
+
+    friends.push(matchData);
+    res.json(bestMatch);
 
 })
